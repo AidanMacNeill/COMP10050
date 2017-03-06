528f647ab35c8e2820dc0db75dcc658deb02607a
AidanMacNeill <aidanmacneill@gmail.com>
2017-03-06 22:27:40 +0000

    WIP commit of main and .h

---
+++ Assignment 2/functions.h
@@ -0,0 +1,42 @@
+/*
+ * functions.h
+ *
+ *  Created on: 6 Mar 2017
+ *      Author: aidan
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+#define MAX_SIZE 50
+#include <stdbool.h>
+
+struct player
+{
+	char name[MAX_SIZE];
+	char type[MAX_SIZE];
+	int health;
+	int magic;
+	int smartness;
+	int strength;
+	int dexterity;
+	int luck;
+	int pos;
+};
+
+struct slot
+{
+	int slotNum;
+	char type[MAX_SIZE];
+	bool occupied;
+};
+
+void assignSlots(int numSlots, char* slotsArray[], struct slot slots[]); //TODO test
+
+void playerPosition(int numPlayers, int numSlots, struct player players[], struct slot slots[]);
+
+void playerInit(int numPlayers, struct player players[], char* playerType[]);
+
+void playerStats(struct player player[], char* playerType[], int numPlayers);
+
+void playerMove(struct player players[], struct slot slots[], int numSlots, int c, int i);
+#endif /* FUNCTIONS_H_ */
---
+++ Assignment 2/main.c
@@ -0,0 +1,86 @@
+/*
+ * main.c
+ *
+ *  Created on: 4 Mar 2017
+ *      Author: aidan
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+ #include "functions.h" // TODO***********
+
+int main(void)
+{
+	setbuf(stdout, NULL); // Allows scanf statements to work with Eclipse
+	srand(time(NULL));
+
+	printf("Welcome to CrossFire, the multiplayer board game! Have Fun!\n");
+	int numPlayers=-1, numSlots, a, c=-1; // Number of players and slots for the game, a counter and a choice
+	printf("How many players will be playing the game? Max: 6\n");
+	while(numPlayers<0)
+	{
+		scanf("%d", &numPlayers);
+		if(numPlayers<2 || numPlayers>6)
+		{
+			printf ("This is an invalid number, please try again.\n");
+			numPlayers=-1;
+		}
+	}
+
+	printf("How many game slots would you like to use? Min: %d, Max: 20\n", numPlayers);
+	scanf("%d", &numSlots);
+	if(numPlayers > numSlots)
+	{
+		numSlots=numPlayers;
+		printf("The number of slots was set to the minimum to allow all players to play\n"); // Prevents there being too few slots for number of players chosen
+	}
+	if (numSlots > 20)
+	{
+		numSlots=20;
+		printf("The number of slots was sent to the maximum allowed value of 20");
+	}
+	printf("\n");
+
+	struct player players[numPlayers]; // Declare structure for each of the players in the game
+	struct slot slots[numSlots]; // Declare structure for the slots in the game
+	char* playerType[4]={"Elf", "Human", "Ogre", "Wizard"}; // Declare pointer arrays for each of the player types and slot types to be assigned later
+	char* slotsType[3]={"Level Ground", "City", "Hill"};
+
+	assignSlots(numSlots, slotsType, slots); // Assigns slot type randomly to each slot
+	playerPosition(numPlayers, numSlots, players, slots); // Assigns players randomly to unique positions
+	playerInit(numPlayers, players, playerType); // Sets up each player type
+
+	printf("All Players\n");
+	for(a=0; a<numPlayers; a++)
+	{
+		playerStats(players[a], playerType);
+		printf("\nPlayer %d\n Name: %s\n Class: %s\n", a+1, players[a].name, players[a].type); // Refer to values in each variable in the struct
+		printf("---Stats---\nMagic: %d\nSmartness: %d\nStrength: %d\nDexterity: %d\nLuck: %d\n", players[a].magic, players[a].smartness, players[a].strength, players[a].dexterity, players[a].luck);
+		printf("---Slots---\n Starting Position: %d\n", players[a].pos);
+	}
+
+	printf("\n---Let the game begin!---\n");
+	for(a=0; a<numPlayers; a++)
+	{
+		printf("%s, would you like to:\n[1]Move\n[2]Attack\n", players[a].name);
+		while(c<0)
+		{
+			if(c==1)
+			{
+				playerMove(players, slots, numSlots, c, a);
+			}
+			else if(c==2)
+			{
+				// TODO attack implementation
+			}
+			else
+			{
+				printf("Invalid choice, please try again.\n");
+				c=-1;
+			}
+		}
+	}
+}
